06f9cf6d0c687ee90081870f4aeaf4834f918d01
Ujjwal Gupta <ujjwalg056@gmial.com>
2022-02-12 21:17:00 +0530

    changed image

--- pages/index.js
+++ pages/index.js
@@ -55,7 +55,7 @@ export default function Index() {
         </div>
         <img
           className="relative top-0 b-auto right-0 pt-16 sm:w-6/12 -mt-48 sm:mt-0 w-10/12 max-h-860-px"
-          src="/img/pattern_nextjs.png"
+          src="/img/ReadWriteOwn_001.png"
           alt="..."
         />
       </section>
